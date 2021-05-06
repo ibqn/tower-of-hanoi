5a5c05b84612cc15a171cb1e7c3d39a7b0f573b3
ibqn <evgen.ibqn@gmail.com>
2021-05-07 00:03:49 +0200

    complete puzzle

--- script.js
+++ script.js
@@ -13,14 +13,20 @@
   };
 
   const handleClick = (event, rod) => {
-    const index = rod?.dataset?.index;
+    const {
+      dataset: { index },
+    } = rod;
     console.log("clicked", index);
 
     const disks = rod.querySelectorAll(".disk");
     const lastDisk = disks[disks.length - 1];
 
     if (selection && selection.index !== index) {
-      const disk = selection.disk;
+      const { disk } = selection;
+
+      if (lastDisk && lastDisk.dataset.index > disk.dataset.index) {
+        return;
+      }
 
       disk.remove();
       rod.append(disk);
@@ -41,6 +47,21 @@
 
   const rods = document.querySelectorAll("#board>.rod");
   console.log("rods", rods);
+
+  const [firstRod] = rods;
+
+  Array.from({ length: 3 })
+    .fill(null)
+    .forEach((_, index) => {
+      const disk = document.createElement("div");
+
+      disk.classList.add("disk");
+      disk.style.setProperty("--disk-index", index);
+      disk.dataset.index = index;
+
+      firstRod.appendChild(disk);
+    });
+
   rods.forEach((rod, index) => {
     rod.dataset.index = index;
     rod.addEventListener("click", (event) => handleClick(event, rod));
