b4ac8d468e49d036ae10946863f0b71d688c189c
ibqn <evgen.ibqn@gmail.com>
2021-05-18 15:58:36 +0200

    wip. switch to webpack

---
+++ src/script.js
@@ -0,0 +1,70 @@
+// (function () {
+import "./style.scss";
+
+const DISK_SELECTED = "disk-selected";
+
+let selection = null;
+
+const toggleSelect = ({ classList }) => {
+  if (classList.contains(DISK_SELECTED)) {
+    classList.remove(DISK_SELECTED);
+  } else {
+    classList.add(DISK_SELECTED);
+  }
+};
+
+const handleClick = (rod) => () => {
+  const {
+    dataset: { index },
+  } = rod;
+  console.log("clicked", index);
+
+  const disks = rod.querySelectorAll(".disk");
+  const lastDisk = disks[disks.length - 1];
+
+  if (selection && selection.index !== index) {
+    const { disk } = selection;
+
+    if (lastDisk && lastDisk.dataset.index > disk.dataset.index) {
+      return;
+    }
+
+    disk.remove();
+    rod.append(disk);
+
+    toggleSelect(disk);
+    selection = null;
+
+    return;
+  }
+
+  if (disks.length === 0) {
+    return;
+  }
+
+  toggleSelect(lastDisk);
+  selection = selection ? null : { index, disk: lastDisk };
+};
+
+const rods = document.querySelectorAll("#board>.rod");
+console.log("rods", rods);
+
+const [firstRod] = rods;
+
+Array.from({ length: 3 })
+  .fill(null)
+  .forEach((_, index) => {
+    const disk = document.createElement("div");
+
+    disk.classList.add("disk");
+    disk.style.setProperty("--disk-index", index);
+    disk.dataset.index = index;
+
+    firstRod.appendChild(disk);
+  });
+
+rods.forEach((rod, index) => {
+  rod.dataset.index = index;
+  rod.addEventListener("click", handleClick(rod));
+});
+// })();
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/script.js",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
+  output: {
+    filename: "script.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.svg$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              encoding: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
